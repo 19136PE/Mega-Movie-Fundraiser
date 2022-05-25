f60f741e7b135f45ddc8cad2c5129842427106e4
19136PE <82184690+19136PE@users.noreply.github.com>
2022-05-25 23:22:37 +0000

    added a name function that asks for a name and checks that it is not blank

--- main.py
+++ main.py
@@ -3,6 +3,20 @@
 
 # functions go here
 
+  #checks that ticket name is not blank
+def not_blank(question, error_message):
+  valid = False
+
+  while not valid:
+    response = input(question)
+
+    #if name is not blank, program continues
+    if response != "":
+      return response
+
+    #if name is blank, show error (& repeat loop)
+    else:
+      print(error_message)
 
 # ************ Main Routine ************
 
@@ -13,15 +27,16 @@
 # loop to get ticket details
 
   # get name (can't be blank)
-
+name = not_blank("Name: ", "Sorry, This can't be blank, please enter your name")
+  
   # get age (between 12-130)
-
+  
   # calculate ticket prices
-
+  
   # loop to ask for snacks
-
+  
   # calculate snack price
-
+  
   # ask for payment method (and apply surcharge if nessesary)
 
 
