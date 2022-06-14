f92821b468f972473bd81f0163ffb0bb1d827f8b
19136PE <82184690+19136PE@users.noreply.github.com>
2022-06-14 03:10:36 +0000

    added ticket prices ect. broke code needs fixing

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def not_blank(question):
       print()
 
 def int_check(question, low_num, high_num):
-  error = "Please enter a whole number between {} and {}".format(low_num, high_num)
+  error = "Please enter your current age".format(low_num, high_num)
 
   valid = False
   while not valid:
@@ -28,28 +28,32 @@ def int_check(question, low_num, high_num):
       response = int(input(question))
       if low_num <= response <= high_num:
         return response
+      elif age < 12:
+        print("Sorry, You are too young for this movie.")
+        continue
+      elif age > 130:
+        print("This is a little old, seems like an error.")
+        continue
       else:
-        print(error)
-        print()
-
+        continue
     except ValueError:
       print(error)
       print()
-
-
+    
 # ****** Main Routine ******
 
 # set-up dictionarys / lists needed to hold data
-
+name = ""
+ticket_count = 0
+MAX_TICKETS = 5
+profit = 0
+age = 0
 # ask user if they have used program before & show instructions if nessesary
 
 # loop to get ticket details
-name = ""
-count = 0
-MAX_TICKETS = 5
-  
-  #get details...
-while MAX_TICKETS != count:
+#get details...
+    # get name (can't be blank)
+while MAX_TICKETS != ticket_count:
   name = not_blank("Name: ")
 
   # get age (between 12-130)
@@ -57,23 +61,35 @@ while MAX_TICKETS != count:
   if name == "xxx":
     print("You have sold {} tickets.  \n"
       "There are {} places still avaliable"
-      .format(count, MAX_TICKETS - count))
-    exit()
-  elif count == MAX_TICKETS - 1:
+      .format(ticket_count, MAX_TICKETS - ticket_count))
+    break
+  elif ticket_count == MAX_TICKETS - 1:
     
-    count += 1
+    ticket_count += 1
     age = int_check("Age: ", 12, 130)
     print("You have sold all the avalible tickets!")
+    print()
   else:
     age = int_check("Age: ", 12, 130)
-    count += 1
+    ticket_count += 1
     print("You have {} seats "
-          "left".format(MAX_TICKETS - count))
-    print()
-  # get name (can't be blank)
+          "left".format(MAX_TICKETS - ticket_count))
+
+  if age < 16:
+    ticket_price = 7.5
+  elif age >65:
+    ticket_price = 10.5
+  else:
+    ticket_price = 6.5
+    
+  profit_made = ticket_price - 5
+  profit += profit_made
+  print("{} : ${:.2f}".format(name, ticket_price))
+  print()
+print("Profit from Tickets: ${:.2f}".format(profit))
 
   # calculate ticket prices
-  
+
   # loop to ask for snacks
   
   # calculate snack price
